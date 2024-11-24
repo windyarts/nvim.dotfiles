9e663f0a44cfc6bb76305e3ca3952be5bfaac9c8
Ziming Miao <ziming@Mac.lan>
2024-11-24 10:03:31 +0800

    update config as result of tweaking

--- lua/ziming/plugins/telescope.lua
+++ lua/ziming/plugins/telescope.lua
@@ -10,17 +10,17 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
---    local transform_mod = require("telescope.actions.mt").transform_mod
---
---    local trouble = require("trouble")
---    local trouble_telescope = require("trouble.sources.telescope")
---
---    -- or create your custom action
---    local custom_actions = transform_mod({
---      open_trouble_qflist = function(prompt_bufnr)
---        trouble.toggle("quickfix")
---      end,
---    })
+    local transform_mod = require("telescope.actions.mt").transform_mod
+
+    local trouble = require("trouble")
+    local trouble_telescope = require("trouble.sources.telescope")
+
+    -- or create your custom action
+    local custom_actions = transform_mod({
+      open_trouble_qflist = function(prompt_bufnr)
+        trouble.toggle("quickfix")
+      end,
+    })
 
     telescope.setup({
       defaults = {
@@ -29,8 +29,8 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            -- ["<C-t>"] = trouble_telescope.open,
+            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+            ["<C-t>"] = trouble_telescope.open,
           },
         },
       },
