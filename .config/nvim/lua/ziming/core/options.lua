2c31fd7ebb76a7d227637fa01e89b6d20938b4b2
ziming <miaoziming@gmail.com>
2025-03-20 16:47:30 +0800

    upload all remains

--- .config/nvim/lua/ziming/core/options.lua
+++ .config/nvim/lua/ziming/core/options.lua
@@ -7,9 +7,9 @@ opt.number = true
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width 
+opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one 
+opt.autoindent = true -- copy indent from current line when starting new one
 
 opt.wrap = false
 
@@ -34,3 +34,6 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
+
+-- scroll
+opt.scrolloff = 3
---
+++ .config/nvim/lua/ziming/plugins/goto-preview.lua
@@ -0,0 +1,11 @@
+return {
+  -- "rmagatti/goto-preview",
+  -- event = "BufEnter",
+  -- config = function()
+  --   local preview = require("goto-preview")
+  --
+  --   preview.setup({
+  --     default_mappings = true,
+  --   })
+  -- end, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+}
